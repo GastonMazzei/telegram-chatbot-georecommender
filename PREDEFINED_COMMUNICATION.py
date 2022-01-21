b9df8acba43ea33fc5ba27aef5ce3f4c593014b2
gaston <gastonmazzei95@gmail.com>
2022-01-21 10:57:36 +0100

    Making it public

---
+++ PREDEFINED_COMMUNICATION.py
@@ -0,0 +1,21 @@
+
+LANGUAGE = ["es","en"][1]
+
+if LANGUAGE=="es":
+	WELCOME_MESSAGE = 'Bienvenido! Al enviar tus coordenadas, te dira '\
+           		'que ciudad te queda mas cerca y cual te queda mas lejos! '\
+      			  'y si supiera por ejemplo'\
+     			' donde esta el medico mas cercano pues te deberia dar su numero!'
+    HELP_MESSAGE = 'Envia tu ubicacion! el bot respondera :-)'
+    ANSWER_MESSAGE = "Este texto aparece antes de la respuesta"
+
+elif LANGUAGE=="en":
+	WELCOME_MESSAGE = 'Welcome! After sending your coordinates, this bot' \
+		' will tell you which city is closest and which one is farther'
+
+    HELP_MESSAGE = 'Send your location! the bot will answer :-)'
+    ANSWER_MESSAGE = 'This text appears before the answer'
+
+
+
+
--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # telegram-chatbot-georecommender
-Telegram chatbot answers to your location with the closest item in a .csv. For example; it's a "closest business" recommender
+Telegram chatbot answers to your location with the closest item in a .csv. For example; it's a "closest (and furthest)" recommender
 
 The chatbot needs
 * a server
 * a database
 * a mission
 
-And currently has
-* no server
-* a database for venezuela-ice-creams
-* a mission for venezuelan-ice-creams
